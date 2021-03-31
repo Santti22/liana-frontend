671a12ce114e8ff7578dd3bcd7320892e08ce265
Santeri Korpinen <n0koja00@students.oamk.fi>
2021-03-31 11:52:51 +0300

    Final changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-  
+  let section = document.querySelector('.icons');
+  let stop = false;
+
+  // Set animation to begin on scroll event
+  window.addEventListener('scroll', (e) => {
+     // Set the height
+     let animation = (window.scrollY + window.innerHeight) >= section.offsetTop;
+     //If animation hasn't been activated start functions
+     if (animation && (stop === false)) {
+        stop = true;
+        $('.value').each(function () {
+           //Setting counter from 0, trigger animation
+           $(this).prop('Counter', 0).animate({
+              // What is being animated
+              Counter: $(this).text()
+           }, {
+              // How long and how is being animated and how
+              duration: 3000,
+              easing: 'swing',
+              step: function(now) {
+                 $(this).text(Math.ceil(now));
+            }
+           });
+        })
+     }
+  })
 });
